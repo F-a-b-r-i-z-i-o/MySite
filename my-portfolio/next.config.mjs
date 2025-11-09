80174920e49cb3b303c73cd0120c828c7fc0a84b
F-a-b-r-i-z-i-o <fabriziofagiolo18@gmail.com>
2025-11-09 15:42:48 +0100

    testst

--- my-portfolio/next.config.mjs
+++ my-portfolio/next.config.mjs
@@ -1,13 +1,9 @@
-const isProd = process.env.NODE_ENV === "production";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? "/MySite" : "",
-  assetPrefix: isProd ? "/MySite/" : "",
 };
 
 export default nextConfig;
